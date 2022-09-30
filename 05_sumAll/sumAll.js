ad3e65de90c1037cdfad44e47d1b39f1009079c0
Tim Hufnagel <thufnagel@student.ccsbali.com>
2022-09-30 12:19:44 +0800

    Ex 4 Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num_1, num_2) {
+    let new_num = 0
+    if (num_1 > num_2){
+        let small_num = num_2
+        let large_num = num_1
+    }
+    else{
+        let small_num = num_1
+        let large_num = num_2
+    }
+
+    for (let i = small_num; i < large_num; i++ ) {
+        new_num = new_num + i
+        return new_num
+    }
 
 };
 
