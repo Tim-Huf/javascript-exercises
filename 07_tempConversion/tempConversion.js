2ac77c4fd58966d3424e8904749a1329ac45d57c
Tim Hufnagel <thufnagel@student.ccsbali.com>
2022-10-03 10:05:31 +0800

    Ex 4 Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit, celsius) {
+  let celsius = (fahrenheit - 32) * (5/9)
+  let rounded = Math.round(celsius * 10) / 10
+  return rounded
 
 };
 
-const ctof = function() {
+const ctof = function(fahrenheit, celsius) {
+  let fahrenheit = (celsius * (5/9) + 32)
+  let rounded = Math.round(fahrenheit * 10) / 10
+  return rounded
 
 };
 
